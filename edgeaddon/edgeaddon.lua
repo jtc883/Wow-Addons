bb8bd4e90f93192957a6bc834969de4c94e0d1f4
jtc883 <jtc883@gmail.com>
2025-10-03 15:16:49 -0400

    Start

---
+++ edgeaddon/edgeaddon.lua
@@ -0,0 +1,39 @@
+print("\124cFF00FF00<EDGE>\124r MyAddon successfully loaded!")
+
+local mainFrame = CreateFrame("Frame", "MyAddonMainFrame", UIParent, "BasicFrameTemplateWithInset")
+mainFrame:SetSize(500, 350)
+mainFrame:SetPoint("CENTER", UIParent, "CENTER", 0, 0)
+mainFrame.TitleBg:SetHeight(30)
+mainFrame.title = mainFrame:CreateFontString(nil, "OVERLAY", "GameFontHighlight")
+mainFrame.title:SetPoint("TOPLEFT", mainFrame.TitleBg, "TOPLEFT", 5, -3)
+mainFrame.title:SetText("Edge Addon")
+mainFrame:Hide()
+
+mainFrame:EnableMouse(true)
+mainFrame:SetMovable(true)
+mainFrame:RegisterForDrag("LeftButton")
+mainFrame:SetScript("OnDragStart", function(self)
+	self:StartMoving()
+end)
+mainFrame:SetScript("OnDragStop", function(self)
+	self:StopMovingOrSizing()
+end)
+
+mainFrame:SetScript("OnShow", function()
+        PlaySound(808)
+end)
+
+mainFrame:SetScript("OnHide", function()
+        PlaySound(808)
+end)
+
+SLASH_MYADDON1 = "/eca"
+SlashCmdList["MYADDON"] = function()
+    if mainFrame:IsShown() then
+        mainFrame:Hide()
+    else
+        mainFrame:Show()
+    end
+end
+
+table.insert(UISpecialFrames, "MyAddonMainFrame")
